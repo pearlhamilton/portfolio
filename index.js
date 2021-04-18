0ee303e5f38c802b2fadedf67e31521e4aa180db
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-18 14:50:46 +0100

    responsive nav bar

---
+++ index.js
@@ -0,0 +1,21 @@
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-link-container");
+    const navLinks = document.querySelectorAll(".nav-link-container li");
+    
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+        
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        burger.classList.toggle("toggle");
+    });
+    
+}
+
+navSlide();
